ba83b057599218d8b69ae2ee3d8a866021a8ae56
Vladislav <parro@yandex.ru>
2021-11-06 16:55:35 +0300

    feat: 01 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname,'text.txt')
+
+fs.readFile(filePath, 'utf-8', (err, content) => {
+    if (err) {
+        throw err
+    }
+
+    console.log(content)
+})
